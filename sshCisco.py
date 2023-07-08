c94d4f7a864c4caa12d43e22fc082bf5d64d6994
MSIforever <s3766984@students.fscj.edu>
2023-07-08 11:10:35 -0700

    made changes for error handling to Timeout, authentication, and SSHexception

--- sshCisco.py
+++ sshCisco.py
@@ -1,9 +1,10 @@
- Quick Script to connect to Cisco devices via SSH
+#Quick Script to connect to Cisco devices via SSH with error handling
 
 import os
 
 from netmiko import ConnectHandler
 from getpass import getpass
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetMikoTimeoutException
 
 USERNAME = input("Please enter you SSH username: ")
 PASS = getpass("Please enter your SSH password: ")
@@ -15,11 +16,18 @@ device = {
     'device_type': 'cisco_ios',
 }
 
-c = ConnectHandler(**device)
+try:
+    c = ConnectHandler(**device)
+    output = c.send_command('show run')
+    f = open('backup.conf', 'x')
+    f.write(output)
+    f.close()
+    
+except (AuthenticationException):
+    print("An authenticaiton error occured while connecting to: " + device['ip'])
+except (SSHException):
+    print("An error occured while connecting to device " + device['ip'] + " via ssh. Is SSH enabled?")
+except (NetMikoTimeoutException):
+    print("The device " + device['ip'] + " timed out when attempting to connect")
 
-output = c.send_command('show run')
 
-f = open('backup.conf', 'x')
-
-f.write(output)
-f.close()
